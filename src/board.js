739e7f07de23d680efc0a337cc3fe49e86d3d601
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-02 16:02:34 +0200

    formatting

--- src/board.js
+++ src/board.js
@@ -1,28 +1,27 @@
-(function(Onsetsu, undefined){
-	Onsetsu.Board = function(size)
-	{
-		this.field = [];
-		for(var i = 0; i < size; i++) {
-			this.field.push([]);
-			for(var j = 0; j < size; j++) {
-				this.field[i].push(Onsetsu.Syllable.getEmpty());
-			};
-		};
-	};
-	
-	Onsetsu.Board.prototype.set = function(x, y, syllable)
-	{
-		this.field[x][y] = syllable;
-	};
-	
-	Onsetsu.Board.prototype.get = function(x, y)
-	{
-		return this.field[x][y];
-	};
-	
-	Onsetsu.Board.prototype.size = function()
-	{
-		return this.field.length;
-	};
-	
+(function(Onsetsu, undefined) {
+
+    Onsetsu.Board = function(size) {
+        this.field = [];
+        for (var i = 0; i < size; i++) {
+            this.field.push([]);
+            for (var j = 0; j < size; j++) {
+                this.field[i].push(Onsetsu.Syllable.getEmpty());
+            }
+        }
+    };
+
+    Onsetsu.Board.prototype.set = function(x, y, syllable)
+    {
+        this.field[x][y] = syllable;
+    };
+
+    Onsetsu.Board.prototype.get = function(x, y)
+    {
+        return this.field[x][y];
+    };
+
+    Onsetsu.Board.prototype.size = function() {
+         return this.field.length;
+    };
+
 })(Onsetsu);
