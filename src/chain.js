8320e3033ef698c54482f2fa4e88cc61aa84e8f0
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-02 16:08:35 +0200

    formatting

--- src/chain.js
+++ src/chain.js
@@ -1,24 +1,21 @@
-(function(Onsetsu, undefined){
-	Onsetsu.Chain = function()
-	{
-		this.spells = [];
-	};
-	
-	Onsetsu.Chain.prototype.execute = function()
-	{
-		_.each(this.spells, function(spell) {
-			spell.execute();
-		});
-	};
+(function(Onsetsu, undefined) {
+    Onsetsu.Chain = function() {
+        this.spells = [];
+    };
 
-	Onsetsu.Chain.prototype.add = function(spell)
-	{
-		this.spells.push(spell);
-	};
-	
-	Onsetsu.Chain.prototype.get = function()
-	{
-		return this.spells;
-	};
-	
+    Onsetsu.Chain.prototype.execute = function() {
+        _.each(this.spells, function(spell) {
+            spell.execute();
+        });
+    };
+
+    Onsetsu.Chain.prototype.add = function(spell) {
+        this.spells.push(spell);
+    };
+
+    Onsetsu.Chain.prototype.get = function()
+    {
+        return this.spells;
+    };
+
 })(Onsetsu);
--- src/spell.js
+++ src/spell.js
@@ -1,22 +1,20 @@
-(function(Onsetsu, undefined){
-	Onsetsu.Spell = function(name, syllables, fn) {
-		this.name = name;
-		this.syllables = syllables;
-		this.fn = fn || function() {};
-	};
-	
-	Onsetsu.Spell.prototype.getCircle = function()
-	{
-		return this.getSyllables().length - 1;
-	};
-	
-	Onsetsu.Spell.prototype.getSyllables = function()
-	{
-		return this.syllables;
-	};
-	
-	Onsetsu.Spell.prototype.execute = function()
-	{
-		return this.fn();
-	};
+(function(Onsetsu, undefined) {
+    Onsetsu.Spell = function(name, syllables, fn) {
+        this.name = name;
+        this.syllables = syllables;
+        this.fn = fn || function() {};
+    };
+
+    Onsetsu.Spell.prototype.getCircle = function() {
+        return this.getSyllables().length - 1;
+    };
+
+    Onsetsu.Spell.prototype.getSyllables = function() {
+        return this.syllables;
+    };
+
+    Onsetsu.Spell.prototype.execute = function() {
+        return this.fn();
+    };
+
 })(Onsetsu);
