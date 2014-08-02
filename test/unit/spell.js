cb400fc71b60050940670feebef8aa6ca1146ed1
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-02 14:09:20 +0200

    tests for spell class

---
+++ test/unit/spell.js
@@ -0,0 +1,33 @@
+test("compose spell", function() {
+	new Onsetsu.Spell("fireball", [Onsetsu.Syllable.getFire(),
+	                               Onsetsu.Syllable.getChi(),
+	                               Onsetsu.Syllable.getNif()
+	                               ]);
+});
+
+test("circle", function() {
+	var fireball = new Onsetsu.Spell("fireball", [Onsetsu.Syllable.getFire(),
+	                               Onsetsu.Syllable.getChi(),
+	                               Onsetsu.Syllable.getNif()
+	                               ]);
+	var greatFireball = new Onsetsu.Spell("great fireball", [Onsetsu.Syllable.getFire(),
+	                                     Onsetsu.Syllable.getChi(),
+	                                     Onsetsu.Syllable.getNif(),
+	                                     Onsetsu.Syllable.getNif(),
+	                                     Onsetsu.Syllable.getGam()
+	                                     ]);
+	
+	assert(fireball.getCircle() === 2);
+	assert(greatFireball.getCircle() === 4);
+});
+
+test("get syllables", function() {
+	var syllables = [Onsetsu.Syllable.getFire(),
+                     Onsetsu.Syllable.getChi(),
+                     Onsetsu.Syllable.getNif()
+                     ];
+	var fireball = new Onsetsu.Spell("fireball", syllables);
+
+	assert(fireball.getSyllables() === syllables);
+});
+
