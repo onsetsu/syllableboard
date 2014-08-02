bcab589731a4428ef5f50409373afcd559c59444
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-02 13:57:16 +0200

    complex syllable matching

--- test/unit/syllable.js
+++ test/unit/syllable.js
@@ -2,12 +2,23 @@ test("get syllables", function() {
 	Onsetsu.Syllable.getPai();
 });
 
-test("matches", function() {
+test("simple matching", function() {
 	var pai = Onsetsu.Syllable.getPai();
 	var pai2 = Onsetsu.Syllable.getPai();
 	var nif = Onsetsu.Syllable.getNif();
 	
 	assert(pai.matches(pai));
 	assert(!(pai.matches(nif)));
+});
+
+test("complex matching", function() {
+	var light = Onsetsu.Syllable.getLight();
+	var sol = Onsetsu.Syllable.getSol();
+	var omnipotency = Onsetsu.Syllable.getOmnipotency();
+	
+	assert(sol.matches(light));
+	assert(!(light.matches(sol)));
+	assert(omnipotency.matches(light));
+	assert(!(omnipotency.matches(sol)));
 });
 
